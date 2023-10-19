c4f702314c64cdbfdb7692d82a8a62868c36283d
Yinlei Meng <mengyl4772@gmail.com>
2023-10-20 00:12:08 +0800

    ******

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/youngfr/code-interview
 
 go 1.20
+
+require github.com/PuerkitoBio/goquery v1.8.1
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
---
+++ scripts/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"unicode"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	resp, err := http.Get("https://pyxt.ustc.edu.cn/?menu=public_replay")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := make([][]string, 0)
+	res = append(res, []string{"id", "title", "name"})
+
+	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
+		row := s.Find("td")
+		if row.Length() == 9 && strings.Contains(row.Eq(2).Text(), "软件工程") {
+			res = append(res, []string{trim(row.Eq(0).Text()), trim(row.Eq(1).Text()), trim(row.Eq(3).Text())})
+		}
+	})
+
+	fd, err := os.OpenFile("titles.csv", os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fd.Close()
+
+	c := csv.NewWriter(fd)
+
+	for _, r := range res {
+		if err := c.Write(r); err != nil {
+			log.Fatal(err)
+		}
+	}
+	c.Flush()
+	if err := c.Error(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Press Enter to exit...")
+	b := make([]byte, 1)
+	os.Stdin.Read(b)
+}
+
+func trim(s string) string {
+	return strings.TrimFunc(s, func(r rune) bool {
+		return unicode.IsSpace(r)
+	})
+}
