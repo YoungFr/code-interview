a935efee9f24e8ca8cc1587dc92e28efd5586298
Yinlei Meng <mengyl4772@gmail.com>
2023-12-03 16:36:58 +0800

    6.824 - fix code format

--- ddbs/crawler.go
+++ ddbs/crawler.go
@@ -79,6 +79,8 @@ func Serial(url string, fetcher Fetcher, fetched map[string]bool) {
 
 // Concurrent crawler with shared state and Mutex
 
+// 所有 goroutine 共享同一个 fetched 哈希表
+// 使用锁来保护它
 type fetchState struct {
 	mu      sync.Mutex
 	fetched map[string]bool
@@ -88,6 +90,9 @@ func makeState() *fetchState {
 	return &fetchState{fetched: make(map[string]bool)}
 }
 
+// 如果线程 T1 和 T2 使用同一个 url 读取 fetched[url] 的值
+// 假设这个 url 还没有被访问过 => 线程 T1 和 T2 都读到 false 值
+// 两个线程就会对同一个 url 进行 Fetch 调用
 func (fs *fetchState) testAndSet(url string) bool {
 	fs.mu.Lock()
 	defer fs.mu.Unlock()
@@ -104,6 +109,7 @@ func ConcurrentMutex(url string, fetcher Fetcher, fs *fetchState) {
 	if err != nil {
 		return
 	}
+	// 使用 sync.WaitGroup 等待所有 goroutine 结束
 	var done sync.WaitGroup
 	for _, url := range urls {
 		done.Add(1)
@@ -130,6 +136,8 @@ func worker(url string, ch chan []string, fetcher Fetcher) {
 func coordinator(ch chan []string, fetcher Fetcher) {
 	n := 1
 	fetched := make(map[string]bool)
+	// 如果接收操作先执行
+	// 接收方 goroutine 会阻塞直到另一个 goroutine 发送一个值
 	for urls := range ch {
 		for _, u := range urls {
 			if !fetched[u] {
@@ -146,8 +154,11 @@ func coordinator(ch chan []string, fetcher Fetcher) {
 }
 
 func ConcurrentChannel(url string, fetcher Fetcher) {
+	// 无缓冲管道
 	ch := make(chan []string)
 	go func() {
+		// 无缓冲管道的发送操作会阻塞
+		// 直到另一个 goroutine 在对应的通道上执行接收操作
 		ch <- append([]string(nil), url)
 	}()
 	coordinator(ch, fetcher)
