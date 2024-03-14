ea416815f1cf3d74b47ebf02482b735ebb8217e7
YoungFr <mengyl4772@gmail.com>
2024-03-14 22:49:44 +0800

    os

--- coroutinepool.go
+++ coroutinepool.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"math/rand"
 	"sync"
 )
 
-// 协程池是一组随时等待分配任务的协程
-// 每个协程接到任务就执行
-// 执行完后继续等待下一次分配
+// 协程池
 
 type Job struct {
 	id    int
@@ -21,28 +18,29 @@ type Result struct {
 
 const capacity = 5
 
+// 全局任务队列
 var jobs = make(chan Job, capacity)
 
+// 向任务队列中添加任务
 func Allocate(njobs int) {
 	for i := 0; i < njobs; i++ {
-		jobs <- Job{id: i, input: rand.Int()}
+		jobs <- Job{id: i, input: i}
 	}
 	close(jobs)
 }
 
+// 全局结果队列
 var results = make(chan Result, capacity)
 
-func worker(wg *sync.WaitGroup) {
-	for job := range jobs {
-		results <- Result{job: job, ouput: work(job.input)}
+// 从结果队列中读取结果
+func GetResult(done chan<- bool) {
+	for result := range results {
+		_, _, _ = result.job.id, result.job.input.(int), result.ouput.(int)
 	}
-	wg.Done()
+	done <- true
 }
 
-func work(input any) any {
-	return input
-}
-
+// 启动 n 个协程并等待它们结束
 func CreatePool(nworkers int) {
 	var wg sync.WaitGroup
 	for i := 0; i < nworkers; i++ {
@@ -53,9 +51,15 @@ func CreatePool(nworkers int) {
 	close(results)
 }
 
-func GetResult(done chan bool) {
-	for result := range results {
-		_, _, _ = result.job.id, result.job.input.(int), result.ouput.(int)
+// 从任务队列中取出任务处理后发送到结果队列
+func worker(wg *sync.WaitGroup) {
+	for job := range jobs {
+		results <- Result{job: job, ouput: work(job.input)}
 	}
-	done <- true
+	wg.Done()
+}
+
+// 对输入进行处理产生输出的函数
+func work(input any) any {
+	return input
 }
